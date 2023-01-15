dbaa40ecf1a1eac07b349b682e14e299b147622e
João Vitor <joaovitor55670@gmail.com>
2023-01-15 16:16:27 -0300

    first-commit

---
+++ script.js
@@ -0,0 +1,31 @@
+//  let p = document.querySelector('#p')
+
+
+
+// let a = 24936390
+
+
+// fetch(`https://viacep.com.br/ws/${a}/json/`).then(r => {
+//     return r.json()
+// }).then(c =>{
+//     console.log(c)
+// })
+
+
+// let submit = document.querySelector('#submit')
+
+// submit.addEventListener('click', function(){
+
+
+//     let cep = document.querySelector('#input').value
+
+//     fetch(`https://viacep.com.br/ws/${cep}/json/`).then(r =>{
+//         return r.json()
+//     }).then(c =>{
+//         console.log(c.bairro)
+        
+//     })
+
+    
+
+// })
