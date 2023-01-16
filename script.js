00f952935bcb0d731e3d4ba7bdb38edcdfde72c4
João Vitor <joaovitor55670@gmail.com>
2023-01-16 17:52:23 -0300

    final-commit

--- script.js
+++ script.js
@@ -1,26 +1,62 @@
+//Botao de de enviar
 let submit = document.querySelector('#submit')
 
+//Adicionando evento de click com uma função 
+submit.addEventListener('click', diglet)
 
-submit.addEventListener('click', diglet => {
+//Inicio da função que será executada com o clique no botão
+function diglet() {
+
 
     let a = document.querySelector('#number').value
 
-    fetch(`https://viacep.com.br/ws/${a}/json/`).then(resposta => {
-        return resposta.json()
-    }).then(body => {
-        let span = document.querySelector('#span')
-        let span2 = document.querySelector('#span2')
-        let span3 = document.querySelector('#span3')
-        let span4 = document.querySelector('#span4')
-        let div = document.querySelector('.answer')
+    //Caso o campo fiquei vazio
+    if (!a) {
+        alert('Campo vazio!')
+    }
+    //Caso tenha a quantidade de números inválida
+    if (a.length > 0 && a.length < 8) {
+        alert('Cep inválido!')
+    }//Fim if
+    else {
+        //pegando oq tem dentro da API e passando pra uma função resposta, que retorna ela msm, ou seja oq ta na API
+        fetch(`https:viacep.com.br/ws/${a}/json/`).then(resposta => {
+            return resposta.json()
+        }).then(body => { //pegando uma parte doq tem dentro da API e colocando na função body
 
-        div.style = 'display:flex'
+            //Caso o número do cep esteja errado
+            if (body.erro == true) {
+                alert('Cep não encontrado!')
+            }//Fim if 
 
-        span.innerHTML = body.logradouro
-        span2.innerHTML = body.bairro
-        span3.innerHTML = body.localidade + '/' + body.uf
-        span4.innerHTML = body.cep
-    })
-})
+            else { //Colocando as informaçoes nos devidos lugares
+                let div = document.querySelector('.answer').style = 'display:flex'
+                let span = document.querySelector('#span').innerHTML = body.logradouro
+                let span2 = document.querySelector('#span2').innerHTML = body.bairro
+                let span3 = document.querySelector('#span3').innerHTML = body.localidade + '/' + body.uf
+                let span4 = document.querySelector('#span4').innerHTML = body.cep
+
+            }
+        })
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
 
 
