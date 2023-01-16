0f0cc5fc1562dee61f1842c0840d5ab0c683979b
João Vitor <joaovitor55670@gmail.com>
2023-01-16 16:25:39 -0300

    style-commit

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ let submit = document.querySelector('#submit')
 
 
 submit.addEventListener('click', diglet => {
-    
+
     let a = document.querySelector('#number').value
-    
+
     fetch(`https://viacep.com.br/ws/${a}/json/`).then(resposta => {
-    return resposta.json()
-}).then(body => {
-    let span = document.querySelector('#span')
-    let span2 = document.querySelector('#span2')
-    let span3 = document.querySelector('#span3')
-    let span4 = document.querySelector('#span4')
-    let div = document.querySelector('.answer')
-    
-    div.style = 'display:flex'
-    
-    span.innerHTML = body.logradouro
-    span2.innerHTML = body.bairro
-    span3.innerHTML = body.localidade + '/' + body.uf
-    span4.innerHTML = body.cep
-})
+        return resposta.json()
+    }).then(body => {
+        let span = document.querySelector('#span')
+        let span2 = document.querySelector('#span2')
+        let span3 = document.querySelector('#span3')
+        let span4 = document.querySelector('#span4')
+        let div = document.querySelector('.answer')
+
+        div.style = 'display:flex'
+
+        span.innerHTML = body.logradouro
+        span2.innerHTML = body.bairro
+        span3.innerHTML = body.localidade + '/' + body.uf
+        span4.innerHTML = body.cep
+    })
 })
 
 
